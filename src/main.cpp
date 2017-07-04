d2a9acb076a99cc920485b2f23a353cd60ac2cf7
Christoph Caprano <caprano@in.tum.de>
2017-07-04 16:11:25 +0200

    Finish main example test

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,44 @@
 #include "roboy_error_detection/roboyErrorDetection.hpp"
 
+void handleDebugNotification(const roboy_communication_control::SystemNotification::ConstPtr &msg) {
+    ROS_INFO("Received debug with object id %d - related to %d", msg->id, msg->objectId);
+}
+
+void handleInfoNotification(const roboy_communication_control::SystemNotification::ConstPtr &msg) {
+    ROS_INFO("Received info with object id %d - related to %d", msg->id, msg->objectId);
+}
+
+void handleWarningNotification(const roboy_communication_control::SystemNotification::ConstPtr &msg) {
+    ROS_INFO("Received warning with object id %d - related to %d", msg->id, msg->objectId);
+}
+
+void handleErrorNotification(const roboy_communication_control::SystemNotification::ConstPtr &msg) {
+    ROS_INFO("Received error with object id %d - related to %d", msg->id, msg->objectId);
+}
+
+void handleDangerNotification(const roboy_communication_control::SystemNotification::ConstPtr &msg) {
+    ROS_INFO("Received error with object id %d - related to %d", msg->id, msg->objectId);
+}
+
+void sendMotorHealthStateMsg(ros::Publisher publisher, bool isMotorHealthy) {
+    int dummyMsgID = 1;
+    int dummyPwmRef = 0;
+    int dummyPosition = 0;
+    short dummyVelocity = 0;
+    short dummyDisplacement = 0;
+    short currentValue = isMotorHealthy ? 1 : 0;
+
+    roboy_communication_middleware::MotorStatus msg;
+    msg.id = dummyMsgID;
+    msg.pwmRef = { dummyPwmRef };
+    msg.position = { dummyPosition };
+    msg.velocity = { dummyVelocity };
+    msg.displacement = { dummyDisplacement };
+    msg.current = { currentValue };
+
+    publisher.publish(msg);
+}
+
 int main(int argc, char* argv[])
 {
     if (!ros::isInitialized()) {
@@ -9,13 +48,41 @@ int main(int argc, char* argv[])
     }
 
     ros::NodeHandlePtr nh = ros::NodeHandlePtr(new ros::NodeHandle);
+    ros::Publisher motor_publisher = nh->advertise<roboy_communication_middleware::MotorStatus>("/roboy/middleware/MotorStatus", 1000);
     RoboyErrorDetection handler(nh);
 
-    int MOTOR_ID = 1;
+    // setup for listening to motor1 health
+    int MOTOR_ID = 0;
     handler.listenForMotorHealth(MOTOR_ID);
 
+    // listening to warning topic to get notified on a warning
+    nh->subscribe(topicAddresses[DEBUG_LEVEL], 1, handleDebugNotification);
+    nh->subscribe(topicAddresses[INFO_LEVEL], 1, handleInfoNotification);
+    nh->subscribe(topicAddresses[WARNING_LEVEL], 1, handleWarningNotification);
+    nh->subscribe(topicAddresses[ERROR_LEVEL], 1, handleErrorNotification);
+    nh->subscribe(topicAddresses[DANGER_LEVEL], 1, handleDangerNotification);
+
+    while (motor_publisher.getNumSubscribers() == 0) {
+        ROS_INFO("Sleep one second until publisher is setup");
+        usleep(1000*1000);
+    }
+
+    int counter = 0;
     while (ros::ok()) {
+        if (counter %2 == 0) {
+            // send an example motor is dead command
+            ROS_INFO("Sends motor is DEAD command");
+            sendMotorHealthStateMsg(motor_publisher, false);
+        } else {
+            // send an example motor is alive command
+            ROS_INFO("Sends motor is ALIVE command");
+            sendMotorHealthStateMsg(motor_publisher, true);
+        }
+
         ROS_INFO_THROTTLE(5, "Listening for Roboy Errors!");
+        ros::spinOnce();
+        ros::Duration(2).sleep();
+        counter++;
     }
 
     return 0;
--- src/roboyErrorDetection.cpp
+++ src/roboyErrorDetection.cpp
@@ -1,9 +1,11 @@
 #include "roboy_error_detection/roboyErrorDetection.hpp"
 
 RoboyErrorDetection::RoboyErrorDetection(ros::NodeHandlePtr nh) {
+    ROS_INFO("Calls constructor");
     notifier.setNodeHandler(nh);
 
-    motorSub = nh->subscribe("/roboy/middleware/MotorStatus", 1, &RoboyErrorDetection::handleMotorStatusErrors, this);
+    motorSub = nh->subscribe("/roboy/middleware/MotorStatus", 1000, &RoboyErrorDetection::handleMotorStatusErrors, this);
+    ROS_DEBUG("Subscribed to /roboy/middleware/MotorStatus");
 };
 
 void RoboyErrorDetection::handleMotorStatusErrors(const roboy_communication_middleware::MotorStatus::ConstPtr &msg) {
@@ -19,9 +21,9 @@ void RoboyErrorDetection::listenForMotorHealth(MotorID motorId, NotificationInte
         subscriptionsForMotorHealth[motorId] = {};
     }
 
-    std::tuple<uint16_t> subscriptionData (minPublishIntervalInMs);
+    NotificationData notificationData (minPublishIntervalInMs);
     // TODO: check what happens if subscription still exists (existing error subscription with 2000ms, what happens if another come with 1000ms?)
-    subscriptionsForMotorHealth[motorId][logLevel] = subscriptionData;
+    subscriptionsForMotorHealth[motorId][logLevel] = notificationData;
 }
 
 void RoboyErrorDetection::handleMotorHealthCheck(const roboy_communication_middleware::MotorStatus::ConstPtr &msg) {
@@ -33,9 +35,11 @@ void RoboyErrorDetection::handleMotorHealthCheck(const roboy_communication_middl
     }
     lastMotorHealthCheckTime = ros::Time::now(); // update time of last motor check --> is current now
 
+    ROS_INFO("Received motor health check message");
     // check all subscribed motors if we should send a health message
     for(auto const &motorEntry : subscriptionsForMotorHealth) {
         MotorID motorId = motorEntry.first;
+        ROS_INFO("Found entry with motor ID %d", motorId);
 
         // loop and process all subscriptions for this motor
         for(auto const &motorSubscriptions : motorEntry.second) {
