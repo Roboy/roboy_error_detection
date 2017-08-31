407c17f0f7cbe3c237c382dda880791fc4c522af
Christoph Caprano <caprano@in.tum.de>
2017-08-31 23:54:39 +0200

    Remove rostopic commands from run-docker.sh

--- bin/run-docker.sh
+++ bin/run-docker.sh
@@ -6,7 +6,5 @@ roscore &
 source /opt/ros/kinetic/setup.bash
 catkin_make
 source devel/setup.bash
-rostopic echo /roboy/system_notification/warning &
-rosrun roboy_error_detection motor_tendent_inconsistence
 
 /usr/sbin/sshd -D
